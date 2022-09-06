87b40ba85432ba81ad43ec58db35cd8084ced22b
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2022-09-06 18:48:45 +0530

    Time: 269 ms (18.24%), Space: 34.2 MB (9.01%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int sum = 0;
+        for(auto x:nums){
+            int y = find_divisor(x);
+            if(y!=-1) sum += y;
+        }
+        return sum;
+    }
+    int find_divisor(int n){
+        vector <int> ans;
+        for(int i = 1; i*i <= n; i++){
+            if(n%i == 0){
+                ans.push_back(i);
+                if(i*i!=n) ans.push_back(n/i);
+            }
+            
+        }
+        // return ans;
+        if(ans.size() == 4){
+            return accumulate(ans.begin(),ans.end(),0);
+        }
+        else{
+            return -1;
+        }
+    }
+};
