f12aa026b19de68d15260e8dab255396416166ff
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2022-10-28 01:56:59 +0530

    Time: 20 ms (65.20%), Space: 6.4 MB (93.90%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -17,24 +17,24 @@ public:
     }
     int minDistance(string s, string t) {
         int n = s.size() , m = t.size();
-        vector <vector <int>> dp(n+1,vector <int>(m+1,0));
+        // vector <vector <int>> dp(n+1,vector <int>(m+1,0));
+        vector <int> prev(m+1),cur(m+1);
         for(int i = 0; i <=m; i++){
-            dp[0][i] = i;
-        }
-        for(int i = 0; i <=n; i++){
-            dp[i][0] = i;
+            prev[i] = i;
         }
         for(int i = 1; i <=n; i++){
+            cur[0] = i;
             for(int j = 1; j <=m; j++){
                 if(s[i-1] == t[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
         }
                  else{
-                        dp[i][j] = min(1 + dp[i][j-1],min(1+ dp[i-1][j],1 + dp[i-1][j-1]));
+                        cur[j] = min(1 + cur[j-1],min(1+ prev[j],1 + prev[j-1]));
         }
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
         
         // return f(n,m,s,t,dp);
     }
