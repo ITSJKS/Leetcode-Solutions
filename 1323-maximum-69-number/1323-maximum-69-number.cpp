492bb8a8076174bbdbd4ba9e4d783cee48bb18bc
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2022-11-07 21:04:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.66%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    int convert_to_num(string num){
-        int ans = 0;
-        for(auto x:num){
-            ans = ans*10 + (x-'0');
-        }
-        return ans;
-    }
     int maximum69Number (int num) {
         string num1 = to_string(num);
         for(auto &x:num1){
@@ -15,7 +8,7 @@ public:
                 break;
             } 
         }
-        return convert_to_num(num1);
+        return stoi(num1);
         
     }
 };
