c6ca8d62d3997bda430a222c00b2ad05684d14d3
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-05 14:47:27 +0530

    Time: 14 ms (79.80%), Space: 8.7 MB (77.40%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,33 +1,43 @@
+#define fi       first
+#define se       second
+#define ll       long long
+#define pb       push_back
+#define pii      pair<int,int>
+#define pll      pair<long long,long long>
+#define vi       vector<int>
+#define vl       vector<long long>
+#define mii      map<int,int>
+#define mll      map<long long,long long>
+#define qi       queue<int>
+#define pqb      priority_queue<int>
+#define pqs      priority_queue<int,vi,greater<int> >
+#define mod      1000000007
+#define inf      INT_MAX
+#define ninf      INT_MIN
+#define ld       long double
+#define f(i,x)     for(int i=0;i<(int)x;i++)
+#define rep(i,a,b)   for(int i=(int)a;i<=(int)b;i++)   
+#define endl      '\n'
+#define all(arr)    arr.begin(),arr.end()
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map <char, int > table;
-        for(auto x:p) table[x]++;
-        int start = 0,end = 0;
-        vector <int> index;
-        int n = s.length();
-        int m = p.length();
-        int counter = table.size();
-        if(m>n || m==0) return index;
-        while(end<n){
-            char ec = s[end];
-            if(table.find(ec)!=table.end()){
-                table[ec]--;
-                if(table[ec] == 0) counter--;
-            }
-            end++;
-            while(counter == 0){
-                if(end - start == m){
-                    index.push_back(start);
-                }
-                char bc = s[start];
-                if(table.find(bc)!=table.end()){
-                    table[bc]++;
-                    if(table[bc]>0) counter++;
-                }
-                start++;
-            }
+        vi ans;
+        if(p.size()>s.size()) return ans;
+        int n = s.size();
+        int m = p.size();
+        vi freq1(26,0);
+        vi freq2(26,0);
+        for(int i = 0; i < p.size(); i++){
+            freq1[s[i]-'a']++;
+            freq2[p[i]-'a']++;
         }
-        return index;
+        if(freq1 == freq2) ans.push_back(0);
+        for(int i = m; i < n; i++){
+            freq1[s[i-m]-'a']--;
+            freq1[s[i]-'a']++;
+            if(freq1 == freq2) ans.push_back(i-m+1);
+        }
+        return ans;
     }
 };
