54c9b18c0b9030f8cf6f848f46fd183d81c32c39
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-06 23:55:51 +0530

    Time: 164 ms (45.23%), Space: 13 MB (60.51%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -12,10 +12,11 @@ public:
         int count = 0;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        for(int i = 0; i <n; i++){
-            for(int j = i+1; j <n; j++){
-                int k = binary_search(nums,j,n,nums[i]+nums[j]);
-                count+=k-j-1;
+        for(int i = 0; i <n-2; i++){
+            int k = i + 2;
+            for(int j = i+1; j <n-1 && nums[i]!=0; j++){
+                while(k < n && nums[i]+nums[j]>nums[k]) k++;
+                count += k - j -1;
             }
         }
         return count;
