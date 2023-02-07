51ffbfa5243465f5970c530e3c1ba02793ccf4f4
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-07 15:57:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (58.83%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    unordered_map <int,int> mp;
     int lastRemaining(int n) {
         if(n == 1) return 1;
-        if(mp.find(n)!=mp.end()) return mp[n];
-        return mp[n] = 2*(n/2-lastRemaining(n/2)+1);
+        return 2*(n/2-lastRemaining(n/2)+1);
     }
 };
