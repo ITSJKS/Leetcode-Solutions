9fb1587846c710762269959c099ec195559a8cc0
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-08 00:41:27 +0530

    Time: 153 ms (48.74%), Space: 57.2 MB (45.81%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
+    bool check(int x, int y){
+        for(int i = 0; i <32; i++){
+            if((x&1<<i) && (y&1<<i)) return false;
+        }
+        return true;
+    }
     int longestNiceSubarray(vector<int>& nums) {
         int n = nums.size();
         int maxi = 0;
         int cur_and = 0;
-        int l = 0;
-        for(int r = 0; r < n; r++){
-            while((cur_and & nums[r]) !=0){
-                    cur_and ^= nums[l++];
-            }
+        for(int r = 0,l = 0; r < n; r++){
+            while(!check(cur_and,nums[r]) && l <= r){
+                    cur_and ^= nums[l];
+                    l++;
+                }
             cur_and  |= nums[r];
-            maxi = max(maxi,r-l+1);         
+            maxi = max(maxi,r-l+1);
+            
         }
         return maxi;
     }
