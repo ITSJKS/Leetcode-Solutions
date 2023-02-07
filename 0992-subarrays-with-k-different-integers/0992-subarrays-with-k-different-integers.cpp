514df350f2ff10515dc333fc790b79c600b03f07
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-07 12:08:37 +0530

    Time: 175 ms (58.42%), Space: 46.4 MB (32.21%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& fruits, int k) {
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atmostK(nums,k) - atmostK(nums,k-1);
+    }
+    
+    int atmostK(vector <int> &nums, int k){
         unordered_map <int,int> mp;
-        int n = fruits.size();
-        int l = 0;
-        int ans = 0;
-        int prefix  = 0;
-        for(int r = 0; r <n; r++){
-            mp[fruits[r]]++;
-            while(mp.size() > k && l <= r){
-               if(mp[fruits[l]] == 1) mp.erase(fruits[l]);
-               l++;
-               prefix = 0;
-                
-            }
-            while(mp[fruits[l]]>1){
-                mp[fruits[l]]--;
+        int n = nums.size();
+        int count = 0;
+        for(int r = 0,l = 0; r <n; r++){
+            mp[nums[r]]++;
+            if(mp.size()<=k) count += r - l + 1;
+            
+            while(mp.size()>k && l<=r){
+                if(mp[nums[l]] == 1) mp.erase(nums[l]);
+                else mp[nums[l]]--;
                 l++;
-                prefix++;
+                if(mp.size()<=k){
+                    count += r - l + 1;
+                    break;
+                }
             }
-            if(mp.size() == k)
-                ans += 1 + prefix;
+            
         }
-        return ans;
+        return count;
+        
     }
 };
