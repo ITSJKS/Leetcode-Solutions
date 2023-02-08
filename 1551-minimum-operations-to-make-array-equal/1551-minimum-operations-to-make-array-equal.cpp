8f53ba069ff4adab7c63985994e27b814abced7f
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-08 16:35:19 +0530

    Time: 2 ms (58.06%), Space: 6 MB (21.42%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    int term(int a, int d, int n){
-        return a + (n-1)*d;
-    }
     int minOperations(int n) {
-        int a = 1,d = 2;
-        if(n%2){
-            int mid = term(a,d,(n/2)+1);
-            int sum = (n/2+1)*(1 + mid)/2;
-            return (n/2 +1)*mid - sum;
-        }
-        int mid1 = term(a,d,(n/2));
-        int mid2 = term(a,d,(n/2+1));
-        int sum = (1 +  mid1)*(n/2)/2;
-        int mid = (mid1+mid2)/2;
-        return n/2*mid - sum;
+        return n*n/4;
     }
 };
