c5d9e6ab00e807308d102e1f611f70a48055ad8c
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-09 16:49:49 +0530

    Time: 236 ms (82.27%), Space: 89.3 MB (57.26%) - LeetHub

--- 2561-rearranging-fruits/2561-rearranging-fruits.cpp
+++ 2561-rearranging-fruits/2561-rearranging-fruits.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
     long long minCost(vector<int>& basket1, vector<int>& basket2) {
-        set <int> st;
-        map <int,int> mp1,mp2;
-        for(auto x:basket1){
-            mp1[x]++;
-            st.insert(x);
-        }
-        for(auto x:basket2){
-            mp2[x]++;
-            st.insert(x);
-        }
-        vector <int> diff1,diff2;
-        for(auto x:st){
-            int cnt1 = mp1[x], cnt2 = mp2[x];
-            int diff = abs(cnt1-cnt2);
-            if(cnt1 == cnt2) continue;
-            if(diff%2){
-                return -1;
-            }
-            if(cnt1>cnt2){
-                for(int i = 0; i <diff/2; i++){
-                    diff1.push_back(x);
-                }
-            }
-            else{
-                for(int i =0; i <diff/2; i++){
-                    diff2.push_back(x);
-                }
+        map <int,int> count;
+        for(auto x:basket1) count[x]++;
+        for(auto x:basket2) count[x]--;
+        long long mini = (count.begin())->first;
+        vector <long long> swaps;
+        for(auto x:count){
+            int elem = x.first, freq = x.second;
+            if(freq%2) return -1;
+            for(int i = 0; i <abs(freq)/2; i++){
+                swaps.push_back(elem);
             }
         }
-        if(diff1.size()!=diff2.size()) return -1;
-        long long count = 0;
-        int mini = *st.begin();
-        int n = diff1.size();
-        reverse(diff2.begin(),diff2.end());
-        for(int i = 0; i <n; i++){
-            count += min(2*mini,min(diff1[i],diff2[i]));
+        long long ans = 0;
+        for(int i = 0; i <swaps.size()/2; i++){
+            ans += min(swaps[i],2*mini);
         }
-        return count;
+        return ans;
     }
 };
