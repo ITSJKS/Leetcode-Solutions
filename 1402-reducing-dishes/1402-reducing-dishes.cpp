be7a83d74b323cdc72b367d0dd848c7bd9e221d6
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-16 16:48:20 +0530

    Time: 53 ms (29.73%), Space: 21 MB (6.25%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    unordered_map <string,int> mp;
-    int solve(int idx, int count,vector <int> &s, int n){
+
+    int solve(int idx, int count,vector <int> &s, int &n,vector <vector <int>> &dp){
         if(idx >= n ) return 0;
-        string mask = to_string(idx) + "#" + to_string(count);
-        if(mp.count(mask)) return mp[mask];
+        if(dp[idx][count]!=-1) return dp[idx][count];
         int take = 0;
-        take = max(take,s[idx]*count + solve(idx+1,count+1,s,n));   
-        int not_take = solve(idx+1,count,s,n);
-        return mp[mask] = max(take,not_take);
+        take = max(take,s[idx]*count + solve(idx+1,count+1,s,n,dp));   
+        int not_take = solve(idx+1,count,s,n,dp);
+        return dp[idx][count] = max(take,not_take);
     }
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
-        // vector <int> dp(n+1,vector <int>(500001,-1));
-        return solve(0,1,satisfaction,n);
+        vector <vector<int>> dp(n+1,vector <int>(n+1,-1));
+        return solve(0,1,satisfaction,n,dp);
     }
 };
