0525a739e58858e9f467ef25eb2d660de484990a
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-19 18:58:55 +0530

    Time: 20 ms (76.07%), Space: 8.2 MB (86.58%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    // unordered_map <pair <int,vector <int>>,int> mp1;
-    bool is_unique(vector <int> &v){
-        for(auto x:v){
-            if(x>1) return false;
+    int create_mask(string x){
+        int mask = 0;
+        for(auto y:x){
+            int c = y-'a';
+            if(mask&1<<c) return -1;
+            else mask |= 1<<c;
         }
-        return true;
+        return mask;
     }
     int maxLength(vector<string>& arr) {
-        vector <int> v(26);
-         return solve(arr,0,v);
-    }
-    int solve(vector <string> &arr, int idx, vector <int> &v){
-        if(idx == arr.size()) return 0;
-        // if(mp1.find({idx,v})!=mp1.end()) return mp1[{idx,v}];
-        // don't take it
-        int best1 = solve(arr,idx+1,v);
-        // take it
-        for(auto x:arr[idx]){
-            v[x-'a']++;
+        int n = arr.size();
+        vector <int> masks;
+        for(auto x:arr){
+            int mask = create_mask(x);
+            if(mask!=-1) masks.push_back(mask);
         }
-        int best2 = arr[idx].size() + solve(arr,idx+1,v);
-        if(!is_unique(v)) best2 = 0;
-        for(auto x:arr[idx]){
-            v[x-'a']--;
+        int m = masks.size();
+        int maxi = 0;
+        for(int i = 0; i < (1<<m); i++){
+            int mask  = 0;
+            bool check = false;
+            for(int j = 0; j <m; j++){
+                if(i&1<<j){
+                    int m = masks[j];
+                    if(m&mask){
+                        check = true;
+                        break;
+                    }
+                    else mask |= m;
+                }
+            }
+            if(!check){
+                maxi = max(maxi,__builtin_popcount(mask));
+            }
         }
-        return max(best1,best2);
+        return maxi;
     }
 };
