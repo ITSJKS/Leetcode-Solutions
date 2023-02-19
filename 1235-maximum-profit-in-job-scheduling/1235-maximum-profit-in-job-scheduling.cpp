f14fe344af1c29f2da7b0d2f63ba5e78187b0ed8
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-19 20:11:15 +0530

    Time: 170 ms (68.28%), Space: 68.2 MB (17.75%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    unordered_map <int,int> mp;
+    int dp[50001];
     int solve(int idx, vector <vector <int>> &jobs,vector <int> &j){
         if(idx >= jobs.size()) return 0;
-        if(mp.count(idx)!=0) return mp[idx];
+        if(dp[idx]!=-1) return dp[idx];
         int not_take = solve(idx+1,jobs,j);
         int st = jobs[idx][0] , et = jobs[idx][1] , profit = jobs[idx][2];
         auto it = lower_bound(j.begin()+idx+1,j.end(),et) - j.begin();
         int take = profit + solve(it,jobs,j);
-        return mp[idx] = max(take,not_take);
+        return dp[idx] = max(take,not_take);
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
+        memset(dp,-1,sizeof dp);
         vector <vector <int>> jobs(n);
         for(int i = 0; i <n; i++){
             jobs[i] = {startTime[i],endTime[i],profit[i]};
