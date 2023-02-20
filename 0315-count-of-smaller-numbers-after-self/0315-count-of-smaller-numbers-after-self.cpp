dc248956110c33205e3ee86e467592180da505d9
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-20 16:55:02 +0530

    Time: 1410 ms (21.09%), Space: 288.7 MB (22.06%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,27 +1,61 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
-template <class T> using ordered_set = tree<T, null_type,
-less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-// Working of pbds
-//     ordered_set <int> a;
-//     a.insert(2);
-//     a.insert(4);
-//     a.insert(3);
-//     a.insert(7);
-//     a.insert(5);
-//     cout << *a.find_by_order(3) << endl; //output is 5
-//     cout << (a.order_of_key(10)) << endl; //output is 5
+
+class MergeSortTree {
+private:
+    vector<vector<int>> tree;
+    int n;
+
+    void build(int l, int r, int node, vector<int>& arr) {
+        if (l == r) {
+            tree[node].push_back(arr[l]);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        build(l, mid, 2 * node, arr);
+        build(mid + 1, r, 2 * node + 1, arr);
+
+        tree[node].resize(r - l + 1);
+        merge(tree[2 * node].begin(), tree[2 * node].end(),
+              tree[2 * node + 1].begin(), tree[2 * node + 1].end(),
+              tree[node].begin());
+    }
+
+    int query(int ql, int qr, int val, int l, int r, int node) {
+        if (l > qr || r < ql) {
+            return 0;
+        }
+
+        if (ql <= l && qr >= r) {
+            return upper_bound(tree[node].begin(), tree[node].end(), val-1) - tree[node].begin();
+        }
+
+        int mid = (l + r) / 2;
+        int left = query(ql, qr, val, l, mid, 2 * node);
+        int right = query(ql, qr, val, mid + 1, r, 2 * node + 1);
+        return left + right;
+    }
+
+public:
+    MergeSortTree(vector<int>& arr) {
+        n = arr.size();
+        tree.resize(4 * n);
+        build(0, n - 1, 1, arr);
+    }
+
+    int query(int l, int r, int val) {
+        return query(l, r, val, 0, n - 1, 1);
+    }
+};
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
+        MergeSortTree st(nums);
         int n = nums.size();
-        ordered_set <pair<int,int>> st;
-        vector <int> ans(n,0);
-        for(int i = n-1; i >=0; i--){
-            ans[i] = st.order_of_key({nums[i],-1});
-            st.insert({nums[i],i});
+        vector <int> ans(n);
+        for(int i = 0; i <n; i++){
+            ans[i] = st.query(i+1,n-1,nums[i]);
         }
         return ans;
     }
