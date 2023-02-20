200de08f6474c2408e68fb288fc9c586787694c9
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-02-20 15:23:16 +0530

    Time: 165 ms (46.02%), Space: 65.3 MB (69.89%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
     int maxJump(vector<int>& stones) {
-        // binary search
-        // we can check if a particular jump is valid by being greedy 
-        auto check = [&](int mid){
-            set <int> st;
-            int cur = 0;
-            while(cur+mid<stones.back()){
-                auto it = upper_bound(stones.begin(),stones.end(),cur+mid);
-                it--;
-                if(*it == cur) return false;
-                cur = *it;
-                st.insert(cur);
-            }
-            vector <int> left_stones;
-            for(auto x:stones){
-                if(st.count(x)==0) left_stones.push_back(x);
-            }
-            int maxi = 0;
-            for(int i =1; i <left_stones.size(); i++){
-                maxi = max(maxi,left_stones[i]-left_stones[i-1]);
-            }
-            return maxi <=mid;
-            
-        };
-        int l = 0, r = stones.back()+1;
-        while(r-l>1){
-            int mid = l + (r-l)/2;
-            if(check(mid)) r = mid;
-            else l = mid;
+        if(stones.size() == 2) return stones[1]-stones[0];
+        int maxi = 0;
+        for(int i = 3; i <stones.size(); i+=2){
+            maxi = max(maxi,stones[i]-stones[i-2]);
         }
-        return r;
+        for(int i = 2; i <stones.size(); i+=2){
+            maxi = max(maxi,stones[i]-stones[i-2]);
+        }
+        return maxi;
     }
 };
