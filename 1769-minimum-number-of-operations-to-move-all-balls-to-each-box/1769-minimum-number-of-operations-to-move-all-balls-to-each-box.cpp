57ebdf663bdcbba27b2ca82e51a921a13e497a5a
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-12 01:07:28 +0530

    Time: 7 ms (85.50%), Space: 8.9 MB (77.83%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,14 +3,15 @@ public:
     vector<int> minOperations(string boxes) {
         int n= boxes.size();
         vector <int> ans(n);
-        for(int i = 0; i <n; i++){
-            if(boxes[i] == '1'){
-                for(int j = 0; j < n; j++){
-                    if(i!=j){
-                        ans[j] += abs(i - j);
-                    }
-                }
-            }
+        for(int i = 0,c = 0,op = 0; i <n; i++){
+            ans[i]+=op;
+            c += boxes[i] == '1'?1:0;
+            op += c;
+        }
+        for(int i = n-1,c = 0,op = 0; i >=0 ; i--){
+            ans[i]+=op;
+            c += boxes[i] == '1'?1:0;
+            op += c;
         }
         return ans;
     }
