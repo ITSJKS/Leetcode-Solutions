0a52bcc5c161ef858d68b7ef394d0c5eb728a71e
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-12 00:02:20 +0530

    Time: 659 ms (14.48%), Space: 8.7 MB (70.00%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,42 +1,25 @@
-class Solution {
-public:
-    int find_beauty(int i, int j,vector <vector <int>> &freq){
-        vector <int> b(26);
-        for(int k = 0; k < 26; k++){
-            b[k] = freq[j][k];
-            int c = 0;
-            if(i>0){
-                c = freq[i-1][k];
-                b[k] -= c;
+class Solution
+{
+    public:
+        int beautySum(string s)
+        {
+            int n = s.length(), sum = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                vector<int> dp(26, 0);
+                dp[s[i] - 'a']++;
+                for (int j = i + 1; j < n; j++)
+                {
+                    dp[s[j] - 'a']++;
+                    int minNum = INT_MAX, maxNum = INT_MIN;
+                    for (int k = 0; k < 26; k++)
+                    {
+                        if (dp[k]) minNum = min(minNum, dp[k]);
+                        if (dp[k]) maxNum = max(maxNum, dp[k]);
+                    }
+                    sum = sum + (maxNum - minNum);
+                }
             }
+            return sum;
         }
-        int mini = INT_MAX;
-        int maxi = 0;
-        for(auto x:b){
-            if(x!=0){
-               maxi = max(maxi,x);
-               mini = min(mini,x); 
-            }
-        }
-        return maxi - mini;
-    }
-    int beautySum(string s) {
-        int n = s.size();
-        vector <vector <int>> freq(n,vector <int>(26,0));
-        for(int i = 0; i <n; i++){
-            freq[i][s[i]-'a']++;
-        }
-        for(int i = 1; i <n; i++){
-            for(int j = 0; j <26; j++){
-                freq[i][j] += freq[i-1][j];
-            }
-        }
-        int count = 0;
-        for(int i = 0; i <n; i++){
-            for(int j = i+1; j < n; j++){
-                count += find_beauty(i,j,freq);
-            }
-        }
-        return count;
-    }
 };
