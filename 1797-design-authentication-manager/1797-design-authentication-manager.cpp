0caf5cee4c9d86f83b95148d1e1db2f3a957542b
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-12 13:53:56 +0530

    Time: 155 ms (23.26%), Space: 30.1 MB (86.46%) - LeetHub

---
+++ 1797-design-authentication-manager/1797-design-authentication-manager.cpp
@@ -0,0 +1,38 @@
+class AuthenticationManager {
+public:
+    unordered_map <string,int> mp;
+    int life_time;
+    AuthenticationManager(int timeToLive) {
+        life_time = timeToLive; 
+    }
+    
+    void generate(string tokenId, int currentTime) {
+        mp[tokenId] = currentTime;
+    }
+    
+    void renew(string tokenId, int currentTime) {
+        if(mp.count(tokenId)){
+            if(mp[tokenId]+life_time<=currentTime){
+            }
+            else mp[tokenId] = currentTime;
+        }
+    }
+    
+    int countUnexpiredTokens(int currentTime) {
+        int count = 0;
+        for(auto x:mp){
+            if(x.second+life_time>currentTime){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager* obj = new AuthenticationManager(timeToLive);
+ * obj->generate(tokenId,currentTime);
+ * obj->renew(tokenId,currentTime);
+ * int param_3 = obj->countUnexpiredTokens(currentTime);
+ */
