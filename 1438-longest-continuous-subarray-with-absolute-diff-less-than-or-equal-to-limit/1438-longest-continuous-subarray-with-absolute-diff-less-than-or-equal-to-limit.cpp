1d26271f21034dbd0bd076920f9c15b8bfe54d3a
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-13 16:06:18 +0530

    Time: 233 ms (30.86%), Space: 78.6 MB (28.03%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,42 +1,12 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-template <class T> using ordered_set = tree<T, null_type,
-less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-// Working of pbds
-//     ordered_set <int> a;
-//     a.insert(2);
-//     a.insert(4);
-//     a.insert(3);
-//     a.insert(7);
-//     a.insert(5);
-//     cout << *a.find_by_order(3) << endl; //output is 5
-//     cout << (a.order_of_key(10)) << endl; //output is 5
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        int ans = 0;
-        int n = nums.size();
-        ordered_set <pair <int,int>> st;
-        for(int l =0, r = 0; r<n; r++){
-            st.insert({nums[r],r});
-            int s = st.size();
-            int maxi = (*st.find_by_order(s-1)).first;
-            int mini = (*st.find_by_order(0)).first;
-            if(maxi - mini <= limit){
-                ans = max(ans,r -l + 1);
-            }
-            else{
-                while(maxi - mini > limit){
-                    st.erase({nums[l],l});
-                     s = st.size();
-                    maxi = (*st.find_by_order(s-1)).first;
-                    mini = (*st.find_by_order(0)).first;
-                    l++;
-                }
-            }
+        multiset <int> m;
+        int l = 0, r= 0 , n = nums.size();
+        for(; r < n; r++){
+            m.insert(nums[r]);
+            if(*m.rbegin() - *m.begin() > limit) m.erase(m.find(nums[l++]));
         }
-        return ans;
+        return r - l;
     }
 };
