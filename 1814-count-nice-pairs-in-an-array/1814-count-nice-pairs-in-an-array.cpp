239cf30974d1e3a3fcaadc119d085356a6946a6e
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-13 15:16:18 +0530

    Time: 267 ms (9.88%), Space: 57 MB (43.82%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rev(int num){
+        string a = to_string(num);
+        reverse(a.begin(),a.end());
+        return stoi(a);
+    }
+    int mod = 1e9 + 7;
+    int countNicePairs(vector<int>& nums) {
+        long long  count = 0;
+        map <int,int> mp;
+        for(auto x:nums){
+            int y = rev(x);
+            count = (count + mp[x-y])%mod;
+            mp[x-y]++;
+        }
+        return count;
+    }
+};
