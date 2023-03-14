c96ac5a9ef0e7d21ace2a70e1deac05dddb6e2ae
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-15 00:14:18 +0530

    Time: 4 ms (48.24%), Space: 11.8 MB (21.16%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -1,28 +1,8 @@
 class Solution {
 public:
     int fillCups(vector<int>& a) {
-        priority_queue <int> pq;
-        for(int i = 0; i <3; i++){
-            if(a[i]) pq.push(a[i]);
-        }
-        int count = 0;
-        while(pq.size()){
-            if(pq.size() >=2){
-                int top1 = pq.top();
-                pq.pop();
-                int top2 = pq.top();
-                pq.pop();
-                count++;
-                top1--;
-                top2--;
-                if(top1) pq.push(top1);
-                if(top2) pq.push(top2);
-            }
-            else{
-                count += pq.top();
-                pq.pop();
-            }
-        }
-        return count;
+        int sum = accumulate(a.begin(),a.end(),0);
+        int maxi = *max_element(a.begin(),a.end());
+        return max(maxi,(sum+1)/2);
     }
 };
