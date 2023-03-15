f08d7836926e271eebd228f0905981f6ed2e15df
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-15 13:37:47 +0530

    Time: 317 ms (25.44%), Space: 100.1 MB (76.92%) - LeetHub

---
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        long long day  =1;
+        map <int,long long> mp;
+        for(auto x:tasks){
+            if(mp[x] > day){
+                day = mp[x];
+                
+            }
+            mp[x] = day + space + 1;
+            day++;
+        }
+        return day-1;
+    }
+};
