b2359e66968c86bd0f7fae97e34b1a4ae5024b04
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-15 13:19:34 +0530

    Time: 455 ms (85.30%), Space: 253.4 MB (7.23%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
+    const int N = 1e6 + 11;
     int minGroups(vector<vector<int>>& inv) {
-        sort(inv.begin(),inv.end());
-        multiset <pair <int,int>> st;
+        vector <int> count(N);
         for(auto x:inv){
-            st.insert({x[0],x[1]});
+            count[x[0]]++;
+            count[x[1]+1]--;
         }
-        int count = 0;
-        for(auto x:inv){
-            pair <int,int> p = {x[0],x[1]};
-            if(st.find(p)==st.end()) continue;
-            count++;
-            st.erase(st.find(p));
-            while(st.lower_bound({p.second+1,p.second+1})!=st.end()){
-                auto it = st.lower_bound({p.second+1,p.second+1});
-                p = *it;
-                st.erase(it);
-            }
+        int cur = 0, maxi = 0;
+        for(auto x:count){
+            cur += x;
+            maxi = max(maxi,cur);
         }
-        return count;
+        return maxi;
     }
 };
-
-/*
-
-Rough:-
-sort by starting time
-whenever we need new group we insert new group with start time and end time of interval , and update end time according to need, if we don't find any group with 
-
-Intuition:-
-
-Explanation:-
-
-*/
