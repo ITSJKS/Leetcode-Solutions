92e0eb817dcb4357bdb5eb3a5486b22eaca296de
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-03-25 22:52:17 +0530

    Time: 1084 ms (15.48%), Space: 96.2 MB (12.55%) - LeetHub

---
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        map <int,int> mp;
+        int n = nums.size();
+        for(int i = 0; i <n; i++){
+            for(int j = i+1; j < n; j++){
+                mp[nums[i]*nums[j]]++;
+            }
+        }
+        long long count = 0;
+        for(auto x:mp){
+            count += 8*(x.second)*(x.second-1)/2;
+        }
+        return count;
+                   
+    }
+};
