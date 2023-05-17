ce7ec73b227c1fc7fd8fa70c434b1ba36d578b80
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-05-17 10:43:40 +0530

    Time: 184 ms (35.58%), Space: 88.1 MB (88.96%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
-        unordered_map <int,int> mp;
-        for(auto &x:nums) mp[x]++;
-        if(mp[1]) return 0;
         int k = numsDivide[0];
         int m = numsDivide.size();
         for(int i =1; i <m; i++){
             k = gcd(k,numsDivide[i]);
         }
-        if(k == 1) return -1;
-        set <int> factors;
-        for(int i = 1; i*i<=k; i++){
-            if(k%i == 0){
-                factors.insert(i);
-                if(i*i != k)
-                    factors.insert(k/i);
-            }
-        }
-        auto count = [&](int mid){
-            int c = 0;
-            for(auto &x:nums) c += (x < mid);
-            return c;
-        };
-        for(auto &x:factors){
-            if(mp[x]) return count(x);
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i <nums.size(); i++){
+            if(k%nums[i] == 0) return i;
         }
         return -1;
+        
     }
 };
