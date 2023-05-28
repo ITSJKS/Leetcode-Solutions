b2b98af332e66fd98a2cbb9eb3bc0fa076cce3aa
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-05-28 18:24:06 +0530

    Time: 75 ms (61.95%), Space: 10.3 MB (60.72%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -14,7 +14,16 @@ public:
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        vector <vector <int>> dp(c+1, vector <int>(c+1,-1));
-        return f(1,c,cuts,dp);
+        vector <vector <int>> dp(c+2, vector <int>(c+2,0));
+        for(int i = c; i >=1; i--){
+            for(int j = i; j<=c; j++){
+                int mini = 1e8;
+        for(int k = i; k <=j; k++){
+            mini = min(mini, cuts[j+1] - cuts[i-1] + dp[i][k-1] + dp[k+1][j]);
+        }
+        dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
     }
 };
