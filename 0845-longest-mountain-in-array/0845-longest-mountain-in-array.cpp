f3ab4af4bc83b4f2e5d3479781cc037f8152c083
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-06-06 18:43:28 +0530

    Time: 27 ms (27.60%), Space: 18.5 MB (81.34%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     int longestMountain(vector<int>& arr) {
-        int n = arr.size();
-        if(n < 3) return false;
-        vector <int> prefix(n,1);
-        int cur_len = 1;
-        for(int i = 1; i < n; i++){
-            if(arr[i] > arr[i-1]){
-                cur_len++;
-            }
-            else cur_len = 1;
-            prefix[i] = cur_len;
-        }
-        vector <int> suffix(n,1);
-        cur_len = 1;
-        for(int i = n-2; i>=0; i--){
-            if(arr[i] > arr[i+1]) cur_len++;
-            else cur_len = 1;
-            suffix[i] = cur_len;
-        }
-        int ans = 0;
-        for(int i = 1; i <n-1; i++){
-            if(prefix[i] > 1 && suffix[i] > 1) ans = max(ans,prefix[i]+suffix[i] -1);
+        int n  =arr.size();
+        int ans = 0, i = 1, upper,lower;
+        while(i < n){
+            upper = 0, lower = 0;
+            while(i < n && arr[i-1] == arr[i]) i++;
+            while(i <  n && arr[i-1] < arr[i]) upper++, i++; // increasing part
+            while(i < n && arr[i-1] > arr[i]) lower++, i++;
+            if(upper && lower) ans = max(ans,upper+lower+1);
         }
         return ans;
     }
 };
-
-// length of longest subarray which is a mountain 
-// keep longest mountain in our current window at idx, if our mountain get discarded at any point , we have to restart again as the current mountain length will be 0 only
