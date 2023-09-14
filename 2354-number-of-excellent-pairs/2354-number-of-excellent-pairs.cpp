ee4ad8d9db41db2ab20cd22aebc22cfab87db986
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-09-15 01:06:28 +0530

    Time: 651 ms (5.02%), Space: 99.9 MB (40.93%) - LeetHub

---
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long countExcellentPairs(vector<int>& nums, int k) {
+        unordered_map <int,int> mp;
+        int c = 0;
+        long long count = 0;
+        set <int> st(nums.begin(),nums.end());
+        for(auto &x:st){
+            int cnt = __builtin_popcount(x);
+            c += (2*cnt >=k);
+            for(int j = k; j<=60; j++){
+                count += mp[j-cnt];
+            }
+            mp[cnt]++;
+        }
+        return 2*1ll*count + c;
+    }
+};
