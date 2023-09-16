87e25cad0b4f6e8e525444f8dc21bfb5eb6a64ab
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-09-16 15:30:43 +0530

    Time: 259 ms (89.85%), Space: 117.3 MB (15.61%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,47 +1,38 @@
 class MedianFinder {
 public:
-    priority_queue<int> max_heap;
-    priority_queue<int,vector<int>,greater<int>> min_heap;
-    
+    priority_queue <int> first;
+    priority_queue <int,vector <int>,greater<int>> second;
+    int size;
+    void maintain(){
+        int size1 = first.size();
+        int size2 = second.size();
+        if(size1 - size2>1){
+            int top = first.top();
+            first.pop();
+            second.push(top);
+        }
+        else if(size2 > size1){
+            int top = second.top();
+            second.pop();
+            first.push(top);
+        }
+    }
     MedianFinder() {
-        
+        size = 0;
     }
     
     void addNum(int num) {
-        if(max_heap.size() > 0 && num > max_heap.top())
-        {
-            min_heap.push(num);
-        }
-        else
-        {
-            max_heap.push(num);
-        }
-        if(max_heap.size() > min_heap.size()+1)
-        {
-            min_heap.push(max_heap.top());
-            max_heap.pop();
-        }
-        if(min_heap.size() > max_heap.size()+1)
-        {
-            max_heap.push(min_heap.top());
-            min_heap.pop();
-        }
+        int size1 = first.size();
+        int size2 = second.size();
+        if(size2 && second.top() <= num) second.push(num);
+        else first.push(num);
+        maintain();
+        size++;
     }
     
     double findMedian() {
-        if(max_heap.size() == min_heap.size())
-        {
-            return (max_heap.top()+min_heap.top())/2.0;
-        }
-        if(max_heap.size() > min_heap.size())
-        {
-            return max_heap.top();
-        }
-        else
-        {
-            return min_heap.top();
-        }
-        
+        if(size&1) return first.top();
+        else return (first.top() + second.top())/(2*1.0);
     }
 };
 
@@ -51,3 +42,8 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+// we can keep two priority_queue
+// one will be max heap and other one will be min heap
+
+// we will also maintain their size , if size of our data is even then we can find top of first and second one and return their average
+// if size of the data is odd then we will return top of the first priority_queue
