60be9fa013d035c322a7c0718be81080c158d977
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-09-18 16:27:20 +0530

    Time: 152 ms (58.80%), Space: 116.2 MB (41.09%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -12,60 +12,41 @@
 struct Node{
     bool isBST = true;
     int sum;
-    int max_left , min_right;
-    Node(int sum_, int l, int r, bool flag){
-        sum = sum_;
-        max_left = l;
-        min_right = r;
-        isBST = flag;
-    }
+    int maxi , mini;
     Node(){
-        
+        sum = 0; // maximum sum till now
+        maxi = INT_MIN; // minimum value in the subtree
+        mini = INT_MAX; // maximum value in the subtree
+        isBST = true; // if current subtree is bst or not
     }
 };
 class Solution {
 public:
-    int maxi = 0;
-    Node* solve(TreeNode* root){
+    int ans = 0;
+    Node solve(TreeNode* root){
         if(root == nullptr){
-            return new Node(0,INT_MIN,INT_MAX,true);
+            return Node();
         }
-        int sum = 0;
-        int left_max = INT_MIN;
-        int right_min = INT_MAX;
-        bool isBST = true;
-        if(root->left){
-            auto left = solve(root->left);
-            sum += left->sum;
-            left_max = left->max_left;
-            isBST&=left->isBST;
-        }
-        if(root->right){
-            auto right = solve(root->right);
-            sum += right->sum;
-            right_min = right->min_right;
-            isBST&=right->isBST;
-        }
-        sum += root->val;
-        cout<<sum<<" "<<left_max<<" "<<right_min<<" "<<isBST<<endl;
-        if(isBST && root->val > left_max && root->val < right_min){
-            maxi = max(maxi,sum);
+        Node current;
+        Node left = solve(root->left);
+        Node right = solve(root->right);
+        int val = root->val;
+        if(left.isBST && right.isBST && val > left.maxi && val < right.mini){
+            current.isBST = true;
+            current.sum = val + left.sum + right.sum;
+            current.maxi = max(val , right.maxi);
+            current.mini = min(left.mini,val);
         }
         else{
-            isBST = false;
+            current.isBST = false;
+            current.sum = max(left.sum , right.sum);
         }
-        int old_left = left_max;
-        int old_right = right_min;
-        left_max = max({root->val,left_max});
-        right_min = min({root->val,right_min});
-        if(old_right!=INT_MAX) left_max = max(left_max,old_right);
-        if(old_left!=INT_MIN) right_min = min(right_min,old_left);
-        return new Node(sum,left_max,right_min,isBST);  
+        ans = max(ans,current.sum);
+        return current;
     }
     int maxSumBST(TreeNode* root) {
         solve(root);
-        if(maxi == 118) return 84;
-        return maxi;
+        return ans;
     }
 };
 
