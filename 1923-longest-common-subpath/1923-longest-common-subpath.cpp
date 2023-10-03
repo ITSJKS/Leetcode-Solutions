b58dca2c08b9309f353011ffb055b7246218c7be
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-10-03 16:51:44 +0530

    Time: 1027 ms (48.63%), Space: 276.1 MB (58.72%) - LeetHub

--- 1923-longest-common-subpath/1923-longest-common-subpath.cpp
+++ 1923-longest-common-subpath/1923-longest-common-subpath.cpp
@@ -1,4 +1,4 @@
-long long mod = 1000000019 ;
+long long mod = 104355181 ;
 long long base = 165131;
 class Solution {
 public:
@@ -10,9 +10,9 @@ public:
         int size = paths.size();
         auto check = [&](int len){
            if(len == 0) return true;
-           unordered_set <int> st;
+           unordered_map <int,vector <int>> st;
            for(int i = 0; i <size && (i == 0 || st.size()); i++){
-               unordered_set <int> cur;
+               unordered_map <int,vector <int>> cur;
                long long cur_hash = 0;
                long long power = 1;
                for(int j = 0; j< paths[i].size(); j++){
@@ -24,15 +24,22 @@ public:
                        power = (power*base)%mod;
                    }
                    if(j >= len-1){
-                       if( i == 0 || st.count(cur_hash)){
-                           cur.insert(cur_hash);
-                           if(i == size-1) return true;
+                       if(i == 0){
+                           cur[cur_hash].push_back(j-len+1);
+                       }
+                       else if(st.count(cur_hash)){
+                           for (auto pos : st[cur_hash])
+                                if (equal(begin(paths[0]) + pos, begin(paths[0]) + pos + len, begin(paths[i]) + j + 1 - len)) {
+                                    cur[cur_hash].push_back(pos);
+                                    break;
+                                }
+
                        }
                    }
                }
                swap(st,cur);
            }
-           return false;
+           return st.size() != 0;
         };
         int l = -1;
         int r = paths[0].size() + 1;
