84b3ccb8f4f9318213e999c34deb0455788e3580
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2023-10-27 12:52:07 +0530

    Time: 443 ms (26.84%), Space: 233.5 MB (17.95%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,53 +1,68 @@
 class Solution {
 public:
-    vector <vector <int>> dp;
-    bool solve(int i, int j, string &s){
-        if(i == j) return true;
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool ans = false;
-        if(j - i == 1){
-            ans =  s[i] == s[j];
-        }
-        else if(s[i] == s[j] && solve(i+1,j-1,s)){
-            ans = true;
-        }
-        else{
-            ans = false;
-        }
-        return dp[i][j] = ans;
-    }
-    // first approach
-    // I am using dynamic programming to find if my given subarray is palindrome
-    // 
     string longestPalindrome(string s) {
-        int n = s.size();
-        dp.resize(n+1,vector <int>(n+1,0));
-        int max_len = 0;
-        int start_idx = 0;
-        for(int i = n-1; i>=0; i--){
-            for(int j = i; j<n; j++){
-                int len = j - i + 1;
-                if(len == 1) dp[i][j] = 1;
-                else if(len == 2) dp[i][j] = s[i] == s[j];
-                else{
-                    dp[i][j] = (s[i] == s[j] ) && dp[i+1][j-1];
-                }
-                if(dp[i][j]){
-                    if(j- i + 1> max_len){
-                        max_len = j - i + 1;
-                        start_idx = i;
+        string t = s;
+        reverse(t.begin(),t.end());
+        return lc_substring(s,t);
+    }
+    string lc_substring(string s, string t){
+        int n = s.length();
+        int m = t.length();
+        int dp[n+1][m+1];
+        for(int i = 0; i <=n; i++){
+            for(int j = 0; j <=m; j++){
+                if(i == 0 || j == 0) dp[i][j] = 0;
+            }
+        }
+        int res = 0;
+        string str = "";
+        for(int i =1; i <=n; i++){
+            for(int j = 1; j <=m; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                     if(res<dp[i][j])
+                    {
+                        string temp=s.substr(i-dp[i][j],dp[i][j]);
+                        string temp2=temp;
+                        reverse(temp2.begin(), temp2.end());
+                        if(temp==temp2) //we just want to check if matched substring is palindrome or not
+                        {
+                        res=dp[i][j];
+                        str=temp;
+                        }
                     }
-                    
+                }
+                else{
+                    dp[i][j] = 0;
                 }
             }
         }
-        return s.substr(start_idx,max_len);
+        return str;
+        // string maxi = "";
+        // string temp = "";
+        // int i = m,j = n;
+        // while(i>0 && j>0){
+        //     if(s[i-1]==t[j-1]){
+        //         temp += s[i-1];
+        //         string temp1 = temp;
+        //         reverse(temp1.begin(),temp1.end());
+        //         if(temp.size() >maxi.size() && temp == temp1){
+        //             maxi = temp;
+        //         }
+        //         i--;
+        //         j--;
+        //     }
+        //     else{
+        //         temp = "";
+        //         if(dp[i-1][j]>dp[i][j-1]){
+        //             i--;
+        //         }
+        //         else{
+        //             j--;
+        //         }
+        //     }
+        // }
+        // reverse(maxi.begin(),maxi.end());
+        // return maxi;
     }
-    // Time complexity is O(n*n) as I am traversing each subarray and for these subarray I am checking if the current subarray is palindrome or not
-    // I am using dynamic programming aproach to check if my current substring is palindrome
-    
 };
-
-// Few more approach 
-// we can use manacher algorithm also
-// In that algorithm we 
