89e28f9e51d77852d04d980a2b0eba1f6b359e76
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2024-01-10 14:27:08 +0530

    Time: 3 ms (77.38%), Space: 6.3 MB (93.57%) - LeetHub

--- 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
+++ 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    long long dp[16][2][2];
-    long long solve(string &s, string &t,int idx, bool flag,bool suff,int limit){
+    long long dp[16][2];
+    long long solve(string &s, string &t,int idx, bool flag,int limit){
         if(s.size()< t.size()) return 0ll;
         if(idx >= s.size()){
-            return suff;
+            return 1ll;
         }
-        if(dp[idx][flag][suff]!=-1) return dp[idx][flag][suff];
+        if(dp[idx][flag]!=-1) return dp[idx][flag];
         bool check = false;
         int start = s.size() - t.size();
-        char ub = flag ? s[idx]  : limit+'0';
+        char ub = limit+'0';
+        if(flag) ub = s[idx];
         long long count = 0;
-        for(char x = '0'; x <= ub && x <=(limit+'0'); x++){
+        for(char x = '0'; x <= ub && x <= (limit+'0'); x++){
             bool new_flag = flag & (x == ub);
-            bool sf = true;
-            if(idx >= start && x != t[idx-start]) {
-                sf = false;
+            if(idx>= start){
+                if(x == t[idx-start]) count += solve(s,t,idx+1,new_flag,limit);
             }
-            count += solve(s,t,idx+1,new_flag,sf&suff,limit);
+            else{
+                count += solve(s,t,idx+1,new_flag,limit);
+            }
         }
-        return dp[idx][flag][suff] = count;
+        return dp[idx][flag] = count;
     }
     long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
         long long val =stol(s);
@@ -27,13 +29,10 @@ public:
         start -= 1;
         string s1 = to_string(start);
         string s2 = to_string(finish);
-        reverse(s1.begin(), s1.end());
-        while(s1.size() < s2.size()) s1 += '0';
-        reverse(s1.begin(), s1.end());
         memset(dp,-1,sizeof dp);
-        long long val1 = solve(s1,s,0,1,1,limit);
+        long long val1 = solve(s1,s,0,1,limit);
         memset(dp,-1,sizeof dp);
-        long long val2 = solve(s2,s,0,1,1,limit);
+        long long val2 = solve(s2,s,0,1,limit);
         return val2-val1;
     }
 };
