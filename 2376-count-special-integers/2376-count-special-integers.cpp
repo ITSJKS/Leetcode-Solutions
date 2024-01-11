4ac77438476ee9a3251e1bf9d7033def3714ea3b
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2024-01-11 23:50:18 +0530

    Time: 944 ms (5.51%), Space: 6.2 MB (93.58%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int dp[11][2][1025];
     int solve(int ind, bool tight, int mask , string &s){
-        if(ind == s.size()) return mask != 0;
-        if(dp[ind][tight][mask]!=-1) return dp[ind][tight][mask];
+        if(ind == s.size()) return 1;
         int count = 0;
-        int ub = tight?(s[ind]-'0'):9;
+        int ub = tight?s[ind]-'0':9;
         for(int i = 0; i<=ub; i++){
             if(mask&(1<<i)) continue;
-            int newMask = (mask == 0 && i == 0 ? mask : (mask | (1 << i)));
-            int nextTight = (tight && i == (s[ind] - '0')) ? 1 : 0;
-            count += solve(ind+1,nextTight,newMask,s);
+            bool new_tight = tight & (i == ub);
+            if(mask == 0 && i == 0){
+                count += solve(ind+1,new_tight,mask,s);
+            }
+            else count += solve(ind+1,new_tight,mask | (1<<i),s);
         }
-        return dp[ind][tight][mask] = count;
+        return count;
     }
     int countSpecialNumbers(int n) {
         string s = to_string(n);
-        memset(dp,-1,sizeof dp);
-        return solve(0,1,0,s);
+        return solve(0,1,0,s)-1;
     }
 };
 // let's assume we have 10 bits
