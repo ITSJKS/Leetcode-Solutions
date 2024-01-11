98d956f59985a0accb715f9c4537c81ff8afbb61
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2024-01-11 15:36:45 +0530

    Time: 786 ms (39.89%), Space: 119.9 MB (38.25%) - LeetHub

---
+++ 2902-count-of-sub-multisets-with-bounded-sum/2902-count-of-sub-multisets-with-bounded-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int solve(int ind, int cur, vector <pair<int,int>>&v,vector <vector <int>> &dp){
+        if(cur < 0) return 0;
+        if(ind >= v.size()) return 1;
+        if(dp[ind][cur]!=-1) return dp[ind][cur];
+        int cnt = v[ind].second;
+        int num = v[ind].first;
+        long long count =  ((solve(ind+1,cur,v,dp) + solve(ind,cur-num,v,dp) - solve(ind+1,cur-(cnt+1)*num,v,dp))%mod+mod)%mod;
+        return dp[ind][cur] = count;
+    }
+    int countSubMultisets(vector<int>& nums, int l, int r) {
+        unordered_map <int,int> mp;
+        for(auto &x:nums) mp[x]++;
+        long long zero = mp[0]+1;
+        mp.erase(0);
+        vector <pair<int,int>> v;
+        for(auto &x:mp) v.push_back(x);
+        int n = mp.size();
+        vector <vector <int>> dp(n,vector <int>(r+1,-1));
+        int left = solve(0,l-1,v,dp);
+        int right = solve(0,r,v,dp);
+        return ((right - left+mod)%mod*1ll*zero)%mod;
+    }
+};
+
+// for finding 
