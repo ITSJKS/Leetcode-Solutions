b7554d481a35afc5ce01f701b50130bd5c03a201
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2024-03-02 11:14:02 +0530

    Time: 106 ms (7.53%), Space: 12.9 MB (8.14%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int f(int i, int j, vector <int> &cuts,vector <vector <int>> &dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int mini = 1e8;
-        for(int k = i; k <=j; k++){
-            mini = min(mini, cuts[j+1] - cuts[i-1] + f(i,k-1,cuts,dp) + f(k+1,j,cuts,dp));
-        }
-        return dp[i][j] = mini;
-    }
     int minCost(int n, vector<int>& cuts) {
-        int c = cuts.size();
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        vector <vector <int>> dp(c+2, vector <int>(c+2,0));
-        for(int i = c; i >=1; i--){
-            for(int j = i; j<=c; j++){
-                int mini = 1e8;
-        for(int k = i; k <=j; k++){
-            mini = min(mini, cuts[j+1] - cuts[i-1] + dp[i][k-1] + dp[k+1][j]);
-        }
-        dp[i][j] = mini;
+        int m = cuts.size();
+        vector <vector <int>> dp(m,vector <int>(m,-1));
+        function <int(int,int)> solve = [&](int start, int end){
+            if(dp[start][end]!=-1) return dp[start][end];
+            if(start > end) return 0;
+            int ans =1e9;
+            for(int ind = start; ind <=end; ind++){
+                int cost = cuts[end+1]-cuts[start-1] + solve(start,ind-1) + solve(ind+1,end);
+                ans = min(ans,cost);
             }
-        }
-        return dp[1][c];
+            return dp[start][end] = ans;
+        };
+        return solve(1,m-2);
     }
 };
+/*
+N units we can cut the stick according to cuts
+cost of one cut is length of the stick on which you are doing the cut
+
+
+
+*/
